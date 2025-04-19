bf0600badb491a8e0fb666cadf6ffa2a726aadcf
Nikita-pyth <siorito321@gmail.com>
2025-04-19 19:23:53 +0300

    fixture json file created. formatting of Task model improved

--- tasks/models.py
+++ tasks/models.py
@@ -30,15 +30,37 @@ class Task(models.Model):
     description = models.TextField()
     deadline = models.DateField(blank=True, null=True)
     is_completed = models.BooleanField(default=False)
+
     priority = models.CharField(
         max_length=10,
         choices=Priority.choices,
-        default=Priority.MEDIUM,
+        default=Priority.MEDIUM
     )
-    type = models.ForeignKey(TaskType, on_delete=models.CASCADE, blank=True, null=True)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks", null=True, blank=True)
-    assignees = models.ManyToManyField(User, related_name="tasks")
-    tags = models.ManyToManyField(Tag, blank=True)
+
+    type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+        blank=True,
+        null=True
+    )
+
+    assignees = models.ManyToManyField(
+        User,
+        related_name="tasks"
+    )
+
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
